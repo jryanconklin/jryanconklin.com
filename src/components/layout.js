dda89910f81c100a970167a6ac39deebf504b251
J. Ryan Conklin <jryanconklin@gmail.com>
2020-01-04 13:49:51 -0800

    Format code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,12 @@
 import React from "react"
-import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import "./layout.css"
 
-const layoutClasses = "d-flex align-items-center justify-content-center min-vh-100 sans";
+const layoutClasses =
+  "d-flex align-items-center justify-content-center min-vh-100 sans"
 
-export default ({ children }) => <div className={layoutClasses}><div className={'text-center'}>{children}</div></div>
+export default ({ children }) => (
+  <div className={layoutClasses}>
+    <div className={"text-center"}>{children}</div>
+  </div>
+)
